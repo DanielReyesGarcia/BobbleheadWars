920a46ecaf6c7bf6d64cce591a973823afa06f1c
Daniel Reyes <danielreyesg94@gmail.com>
2022-09-30 18:01:04 -0400

    Chapter 5 - fixed bug where player wouldn't turn while moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,33 +25,36 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+       
 
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-            if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
-            {
-                if (hit.point != currentLookTarget)
-                {
-                    currentLookTarget = hit.point;
-                }
-
-                Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-
-                Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
-
-                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
-            }
+            // todo
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
 
-        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+
+            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+            Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+        }
+
     }
 }
